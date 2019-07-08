82696866dab5c43ecda9771c669b9f4a38db0bd5
aishwarya <pappulaaishwarya@gmail.com>
2019-07-08 12:34:06 +0530

    modified

---
+++ src/main/java/com/stackroute/pe4/ReplaceString.java
@@ -0,0 +1,16 @@
+package com.stackroute.pe4;
+public class ReplaceString
+{
+
+    // Variables Declaration
+    String input="",output="";
+
+    public String checkReplaceString(String input)
+    {
+
+        output=input.replace('d','f');    // Replacing 'd' with 'f'
+        output=output.replace('l','t');
+
+        return output;
+    }
+}
